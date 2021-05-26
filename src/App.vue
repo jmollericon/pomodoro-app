a59ce74def7f7bca7ac2693089a2db1cbd282f40
Jorge Mollericon <jlmollericon@gmail.com>
2021-05-26 10:46:40 -0400

    :art: App.Vue define styles, css variables

--- src/App.vue
+++ src/App.vue
@@ -9,24 +9,24 @@
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+
+  // CSS variables
+  // Colors
+  --app-color-primary: #E23434;
+  --app-color-accent: #333745;
+  --app-color-text: #FFFFFF;
+}
+* {
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
