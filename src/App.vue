7dd198a70b024ee13751abbe2b7c5008f2455b34
Jorge Mollericon <jlmollericon@gmail.com>
2021-05-26 11:27:57 -0400

    :zap: firt page done
    use components, layout.scss and others

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <router-view class="app-container"/>
   </div>
 </template>
 
 <style lang="scss">
 @import '@/styles/reset';
+@import '@/styles/layout';
 
 body {
   // CSS variables
---
+++ src/views/Welcome.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="welcome">
+    <div class="app-content welcome-content">
+      <h1 class="welcome-title">Hello, ¡welcome to pomodoro!</h1>
+      <h2 class="welcome-sutitle">Improve your productivity by focusing
+        on what is important in each moment and be more efficient.
+      </h2>
+      <Button @click="onClick()">
+        Make your first pomodoro
+      </Button>
+    </div>
+  </section>
+
+</template>
+
+<script>
+import Button from '@/components/Button.vue';
+
+export default {
+  name: 'Welcome',
+  components: {
+    Button,
+  },
+  methods: {
+    onClick() {
+      console.log('logo clickeado');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.welcome {
+  background-color: var(--app-color-primary);
+}
+.welcome-content {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: var(--app-color-text);
+}
+.welcome-title {
+  font-size: 2rem;
+  font-weight: 500;
+  letter-spacing: 1px;
+  margin-bottom: 1.2rem;
+}
+.welcome-sutitle {
+  font-size: 1.2rem;
+  font-weight: 300;
+  margin-bottom: 1.2rem;
+}
+</style>
